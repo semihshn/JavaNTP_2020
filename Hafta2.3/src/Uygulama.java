31d139a4806489fb6e98db122d8cfb1c7ed627d7
ISTE-BM <ISTE-BM@DESKTOP-SSTU496.mynet>
2020-11-19 20:20:37 +0300

    Kosinus teoremi Uygulamasi

---
+++ Hafta2.3/src/Uygulama.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+
+public class Uygulama {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner tara=new Scanner(System.in);
+		System.out.print("1.kenar:");
+		double a= tara.nextDouble();
+		System.out.print("2.kenar:");
+		double b=tara.nextDouble();		
+		System.out.print("aci:");
+		double aci=tara.nextDouble();
+		
+		Uygulama obje=new Uygulama();
+		obje.Kosinus(a, b, aci);
+		
+	}
+	public void Kosinus(double a, double b, double aci){
+		//Math.cos(Radyan): Derece cinsinden degildir
+		double aciRadyan=Math.toRadians(aci);
+		double sonuc=Math.pow(a, 2)+ b*b-(2*a*b*Math.cos(aciRadyan));
+		System.out.println("3.kenar uzunlugu cos teoremi ile: "+ sonuc);
+	}
+
+}
