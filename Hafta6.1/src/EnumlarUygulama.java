bceb6f074ec25703bd4ddd95621ce3310b436ea5
Semih <57491762+semihshn@users.noreply.github.com>
2021-01-21 11:32:46 +0300

    Update EnumlarUygulama.java

--- Hafta6.1/src/EnumlarUygulama.java
+++ Hafta6.1/src/EnumlarUygulama.java
@@ -2,7 +2,7 @@
 public class EnumlarUygulama {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub.
 		System.out.println(Islemler.Carp.Hesapla(15, 5));
 	}
 
